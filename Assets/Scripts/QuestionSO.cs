a88ad21535ebe588aef259952e2205d39ec61fa4
odrmasliuk <odr.masliuk@gmail.com>
2022-11-05 20:58:03 +0100

    Lecture 4 | Done

---
+++ Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [SerializeField, TextArea(2,5)] private string _question;
+}
