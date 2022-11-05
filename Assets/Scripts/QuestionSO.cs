eba7b8f207eaa5468a9aa5a9b2a895076d03f390
odrmasliuk <odr.masliuk@gmail.com>
2022-11-05 21:37:11 +0100

    Lecture 6 | Done

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
     [SerializeField, TextArea(2,5)] private string _question;
+    [SerializeField] private string[] _answers;
+    [SerializeField] private int _correctAnswerIndex; 
+
     public string Question => _question;
+    public int CorrectAnswerIndex => _correctAnswerIndex;
+    
+    public string GetAnswer (int index) => _answers[index];
+
+    private void OnValidate() 
+    {
+        if (_answers.Length > 0 && _correctAnswerIndex > _answers.Length - 1)
+        {
+            _correctAnswerIndex = _answers.Length - 1;
+        }
+        else if (_answers.Length == 0 || _correctAnswerIndex < 0)
+        {
+            _correctAnswerIndex = 0;
+        }
+    }
 }
