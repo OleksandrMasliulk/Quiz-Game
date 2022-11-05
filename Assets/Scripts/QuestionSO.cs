428fe62eb85a2d910ed56ab3c8b61fc40024894c
odrmasliuk <odr.masliuk@gmail.com>
2022-11-05 22:19:33 +0100

    Lecture 8 | Done

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
     [SerializeField, TextArea(2,5)] private string _question;
-    [SerializeField] private string[] _answers;
+    [SerializeField] private string[] _answers = new string[4];
     [SerializeField] private int _correctAnswerIndex; 
 
     public string Question => _question;
     public int CorrectAnswerIndex => _correctAnswerIndex;
-    
-    public string GetAnswer (int index) => _answers[index];
+
+    public string GetAnswer(int index) => _answers[index];
 
     private void OnValidate() 
     {
--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -4,11 +4,17 @@ using TMPro;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _questionTextField;
+    [SerializeField] private GameObject[] _answerButtons;
 
     [SerializeField] QuestionSO _question;
 
     void Start()
     {
         _questionTextField.text = _question.Question;
+
+        for (int i = 0; i < _answerButtons.Length; i++) 
+        {
+            _answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = _question.GetAnswer(i);
+        }
     }
 }
