c470807a46043135c2d75de59421b3d7ff90b34f
odrmasliuk <odr.masliuk@gmail.com>
2022-11-07 12:06:25 +0100

    Lecture 9 | Done

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _questionTextField;
     [SerializeField] private GameObject[] _answerButtons;
+    [SerializeField] private Sprite _correctAnswerSprite;
+    [SerializeField] private Sprite _wrongAnswerSprite;
 
     [SerializeField] QuestionSO _question;
 
@@ -15,6 +18,25 @@ public class Quiz : MonoBehaviour
         for (int i = 0; i < _answerButtons.Length; i++) 
         {
             _answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = _question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index) 
+    {
+        Image buttonImage = _answerButtons[index].GetComponent<Image>();
+        if (index == _question.CorrectAnswerIndex) 
+        {
+            _questionTextField.text = "Correct!";
+            buttonImage.sprite = _correctAnswerSprite;
+        }
+        else
+        {
+            int correctAnswerIndex = _question.CorrectAnswerIndex;
+            string correctAnswerText = _question.GetAnswer(correctAnswerIndex);
+            _questionTextField.text = $"The correct answer was:\n{correctAnswerText}";
+            buttonImage.sprite = _wrongAnswerSprite;
+            Image correctButtonImage = _answerButtons[correctAnswerIndex].GetComponent<Image>();
+            correctButtonImage.sprite = _correctAnswerSprite;
         }
     }
 }
