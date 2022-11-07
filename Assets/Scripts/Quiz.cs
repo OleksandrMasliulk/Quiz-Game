bd09fd20a9b168e02476438e8528edaa4068a286
odrmasliuk <odr.masliuk@gmail.com>
2022-11-07 12:43:53 +0100

    Lecture 10 | Done

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -6,12 +6,19 @@ public class Quiz : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _questionTextField;
     [SerializeField] private GameObject[] _answerButtons;
-    [SerializeField] private Sprite _correctAnswerSprite;
-    [SerializeField] private Sprite _wrongAnswerSprite;
+    [SerializeField] private Sprite _defaultButtonSprite;
+    [SerializeField] private Sprite _correctButtonSprite;
+    [SerializeField] private Sprite _wrongButtonSprite;
 
     [SerializeField] QuestionSO _question;
 
     void Start()
+    {
+        GetNextQuestion();
+        //SetupQuestion();
+    }
+
+    private void SetupQuestion() 
     {
         _questionTextField.text = _question.Question;
 
@@ -21,22 +28,48 @@ public class Quiz : MonoBehaviour
         }
     }
 
+    private void GetNextQuestion() 
+    {
+        SetButtonsState(true);
+        SetDefaultButtonSprites();
+        SetupQuestion();
+    }
+
     public void OnAnswerSelected(int index) 
     {
         Image buttonImage = _answerButtons[index].GetComponent<Image>();
         if (index == _question.CorrectAnswerIndex) 
         {
             _questionTextField.text = "Correct!";
-            buttonImage.sprite = _correctAnswerSprite;
+            buttonImage.sprite = _correctButtonSprite;
         }
         else
         {
             int correctAnswerIndex = _question.CorrectAnswerIndex;
             string correctAnswerText = _question.GetAnswer(correctAnswerIndex);
             _questionTextField.text = $"The correct answer was:\n{correctAnswerText}";
-            buttonImage.sprite = _wrongAnswerSprite;
+            buttonImage.sprite = _wrongButtonSprite;
             Image correctButtonImage = _answerButtons[correctAnswerIndex].GetComponent<Image>();
-            correctButtonImage.sprite = _correctAnswerSprite;
+            correctButtonImage.sprite = _correctButtonSprite;
+        }
+        SetButtonsState(false);
+    }
+
+    private void SetButtonsState(bool state) 
+    {
+        foreach(GameObject go in _answerButtons) 
+        {
+            Button button = go.GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    private void SetDefaultButtonSprites() 
+    {
+        foreach(GameObject go in _answerButtons) 
+        {
+            Image image = go.GetComponent<Image>();
+            image.sprite = _defaultButtonSprite;
         }
     }
 }
